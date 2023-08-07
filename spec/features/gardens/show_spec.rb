5512a952e78ebfbb6ccc6805cddd806151d09094
Bradley Milton <bmilton2412@gmail.com>
2023-08-07 10:58:12 -0700

    complete user story 3

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -1,30 +1,38 @@
 require "rails_helper"
 
-RSpec.describe Garden, type: :model do
+RSpec.describe "Garden show page" do
   before(:each) do
     @garden1 = Garden.create!(name: "garden1", organic: true)
-
+  
     @p1 = Plot.create!(number: "1", size: "small", direction: "North", garden_id: @garden1.id)
     @p2 = Plot.create!(number: "2", size: "medium", direction: "South", garden_id: @garden1.id)
-
+  
     @plant1 = Plant.create!(name: "strawberry", description: "sweet", days_to_harvest: 1)
     @plant2 = Plant.create!(name: "pepper", description: "hot", days_to_harvest: 2)
     @plant3 = Plant.create!(name: "peach", description: "juicy", days_to_harvest: 101)
-
+  
     @plant_plot1 = PlantPlot.create!(plot: @p1, plant: @plant1)
     @plant_plot2 = PlantPlot.create!(plot: @p1, plant: @plant2)
     @plant_plot3 = PlantPlot.create!(plot: @p2, plant: @plant3)
     @plant_plot4 = PlantPlot.create!(plot: @p2, plant: @plant1)
-  end
-  describe 'relationships' do
-    it { should have_many(:plots) }
-    it { should have_many(:plants).through(:plots) }
-  end
-
-  describe "under_100_days" do
-    it "should return plants with under 100 days to harvest" do
-      expect(@garden1.under_100_days).to eq([@plant1, @plant2])
-      expect(@garden1.under_100_days).to_not eq([@plant3])
+  
+    visit garden_path(@garden1)
     end
+
+  it "should show all plants in the garden with no duplicates and under 100 days to harvest" do
+
+    expect(page).to have_content("Garden plants under 100 days to harvest")
+    expect(page).to have_content(@plant1.name)
+    expect(page).to have_content(@plant2.name)
+    expect(page).to_not have_content(@plant3.name)
   end
 end
+
+
+# User Story 3, Garden's Plants
+
+# As a visitor
+# When I visit a garden's show page ('/gardens/:id')
+# Then I see a list of plants that are included in that garden's plots
+# And I see that this list is unique (no duplicate plants)
+# And I see that this list only includes plants that take less than 100 days to harvest
--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -2,70 +2,55 @@ require "rails_helper"
 
 RSpec.describe "plots index" do
   before(:each) do
-  @garden1 = Garden.create!(name: "garden1", organic: true)
+    @garden1 = Garden.create!(name: "garden1", organic: true)
 
-  @p1 = Plot.create!(number: "1", size: "small", direction: "North", garden_id: @garden1.id)
-  @p2 = Plot.create!(number: "2", size: "medium", direction: "South", garden_id: @garden1.id)
+    @p1 = Plot.create!(number: "1", size: "small", direction: "North", garden_id: @garden1.id)
+    @p2 = Plot.create!(number: "2", size: "medium", direction: "South", garden_id: @garden1.id)
 
-  @plant1 = Plant.create!(name: "strawberry", description: "sweet", days_to_harvest: 1)
-  @plant2 = Plant.create!(name: "pepper", description: "hot", days_to_harvest: 2)
-  @plant3 = Plant.create!(name: "peach", description: "juicy", days_to_harvest: 3)
+    @plant1 = Plant.create!(name: "strawberry", description: "sweet", days_to_harvest: 1)
+    @plant2 = Plant.create!(name: "pepper", description: "hot", days_to_harvest: 2)
+    @plant3 = Plant.create!(name: "peach", description: "juicy", days_to_harvest: 3)
 
-  @plant_plot1 = PlantPlot.create!(plot: @p1, plant: @plant1)
-  @plant_plot2 = PlantPlot.create!(plot: @p1, plant: @plant2)
-  @plant_plot3 = PlantPlot.create!(plot: @p2, plant: @plant3)
-  @plant_plot4 = PlantPlot.create!(plot: @p2, plant: @plant1)
+    @plant_plot1 = PlantPlot.create!(plot: @p1, plant: @plant1)
+    @plant_plot2 = PlantPlot.create!(plot: @p1, plant: @plant2)
+    @plant_plot3 = PlantPlot.create!(plot: @p2, plant: @plant3)
+    @plant_plot4 = PlantPlot.create!(plot: @p2, plant: @plant1)
 
-  visit plots_path
+    visit plots_path
   end
+
   it "should have list of all plot numbers with name of all plots names" do
-    # save_and_open_page
     expect(page).to have_content(@p1.number)
     expect(page).to have_content(@p2.number)
-  
+
     expect(page).to have_content(@plant1.name)
     expect(page).to have_content(@plant2.name)
     expect(page).to have_content(@plant3.name)
-
   end
 
   it "should remove a plant from a plot when clicking the 'Remove' link" do
-    within("#plot1") do
-      expect(page).to have_link("Remove #{@plant1.name} from plot #{@p1.number}")
-      expect(page).to have_link("Remove #{@plant2.name} from plot #{@p1.number}")
-      click_link("Remove #{@plant1.name} from plot #{@p1.number}")
+    within("#plot_#{@p1.id}") do
+      expect(page).to have_link("Remove #{@plant1.name}")
+      expect(page).to have_link("Remove #{@plant2.name}")
+      click_link("Remove #{@plant1.name}")
       expect(current_path).to eq(plots_path)
       expect(page).to_not have_content(@plant1.name)
-      save_and_open_page
     end
 
-
-    within("#plot2") do
+    within("#plot_#{@p2.id}") do
       expect(page).to have_link(@plant1.name)
     end
   end
 
   it "should still show other associated plants on the plot after removal" do
-    within("#plot1") do
+    within("#plot_#{@p1.id}") do
       expect(page).to have_link(@plant2.name)
     end
 
-    within("#plot2") do
+    within("#plot_#{@p2.id}") do
       expect(page).to have_link(@plant1.name)
       expect(page).to have_link(@plant3.name)
     end
   end
 end
 
-# User Story 2, Remove a Plant from a Plot
-
-# As a visitor
-# When I visit the plots index page
-# Next to each plant's name
-# I see a link to remove that plant from that plot
-# When I click on that link
-# I'm returned to the plots index page
-# And I no longer see that plant listed under that plot,
-# And I still see that plant's name under other plots that is was associated with.
-
-# Note: you do not need to test for any sad paths or implement any flash messages. 
